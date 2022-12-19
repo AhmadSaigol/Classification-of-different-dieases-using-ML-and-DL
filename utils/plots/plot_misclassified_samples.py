9a3c7927f09bcee4c45d002b3216952628225a53
Ahmad Saigol <ahmad.saigol@tuhh.de>
2022-12-20 00:26:11 +0100

    initial draft of plotting misclassified samples

--- utils/plots/plot_misclassified_samples.py
+++ utils/plots/plot_misclassified_samples.py
@@ -16,14 +16,20 @@ def plot_MS(y_true, y_pred, path_to_results, path_to_images):
     path_to_images: folder containing images
 
     """
+    num_samples_to_plot = 2
     classes = np.unique(y_true[:,1])
     num_classes = len(classes)
 
-    fig, axes = plt.subplots(num_classes,num_classes, figsize=(15,15))
+    fig, axes = plt.subplots(num_classes, num_samples_to_plot, figsize=(15,15))
     
-    
+    class_no = 0
+    while (class_no != num_classes-1):
+        
+        for i in range(y_true.shape[0] ):
+            #y[i, ]
+            pass
 
-    plt.savefig(path_to_results + "_CM")
+    #plt.savefig(path_to_results + "_CM")
 
 if __name__ == "__main__":
     path_to_txt = "/home/ahmad/Documents/TUHH/Semester 3/Intelligent Systems in Medicine/Project/Classification-of-different-dieases-using-ML-and-DL/results/histogram_new/multiclass/train/0/valid_svm.txt"
