bb9e63f5761b10630e27581955e8d7b1b73f012d
Ahmad Saigol <ahmad.saigol@tuhh.de>
2023-03-13 23:46:53 +0100

    cleaned up results

--- utils/classifiers/SVM.py
+++ utils/classifiers/SVM.py
@@ -107,7 +107,7 @@ def svm(X, parameters, classes, y, save_model_path=None, path_to_model=None):
         
         # set stopping criteria
         model.setTermCriteria((cv2.TERM_CRITERIA_MAX_ITER, 1000, 1e-6))
-
+        
         # can also class weights in case of misclassification of a class
         # svm.setClassWeights ()
     
